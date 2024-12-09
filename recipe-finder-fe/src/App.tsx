2cf478f7dfdf492ae117f7b5cb926226c6b36703
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-09 15:15:58 +0000

    started hero

--- recipe-finder-fe/src/App.tsx
+++ recipe-finder-fe/src/App.tsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+<section>
+  <div className="">
+    <h1>Food Hub</h1>
+  </div>
+</section>
+  </>;
 }
 
 export default App;
