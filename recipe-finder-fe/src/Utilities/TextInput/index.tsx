4441fa6b0e0e1b23f575e8f0c3f2e46dde9415e7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-09 14:50:56 +0000

    text input component created

---
+++ recipe-finder-fe/src/Utilities/TextInput/index.tsx
@@ -0,0 +1,13 @@
+export default function TextInput() {
+  return (
+    <>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Text here"
+        className="w-10/12 border-2 rounded-lg px-2 py-1"
+      />
+    </>
+  );
+}
