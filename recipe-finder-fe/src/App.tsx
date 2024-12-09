05a8db61d5226b4789944c1853e667b2a494fc84
DBaldwin8 <darren_baldwin@live.com>
2024-12-09 15:26:15 +0000

    added routes for '/' to Login and 'recipes' to AllRecipes components

--- recipe-finder-fe/src/App.tsx
+++ recipe-finder-fe/src/App.tsx
@@ -3,15 +3,23 @@ import "./App.css";
 import TextInput from "./Utilities/TextInput";
 import UserContext from "./Contexts/UserContext";
 import Login from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AllRecipes from "./Pages/AllRecipes";
 
 function App() {
   const [currentUserId, setUserId] = useState(0);
 
   return (
     <>
-      <UserContext.Provider value={{ userId: currentUserId, changeUserID: setUserId }} >
-        <Login></Login>;
-        <TextInput />
+      <UserContext.Provider value={{ userId: currentUserId, changeUserID: setUserId }}>
+
+        <BrowserRouter>
+
+        <Routes>
+          <Route path='/' element={<Login></Login>} />
+          <Route path='recipes' element={<AllRecipes></AllRecipes>} />
+        </Routes>
+        </BrowserRouter>
       </UserContext.Provider>
     </>
   );
