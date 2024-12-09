1672dcbfc7ed9987c15df4294af33c35212be4da
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-09 17:28:24 +0000

    end of day commit

--- recipe-finder-fe/src/App.tsx
+++ recipe-finder-fe/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>
-<section>
-  <div className="">
-    <h1>Food Hub</h1>
-  </div>
-</section>
-  </>;
+  return (
+    <>
+      <section className="w-full mt-2">
+        <div className=" md:h-64  bg-cover sm:bg-cover bg-no-repeat bg-[url('../public/img/hero.jpg')] ">
+          <h1 className="text-5xl sm:text-7xl font-sans font-semibold w-full text-center py-16">FoodHub</h1>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
