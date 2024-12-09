2f4a4702a42e2653f72277075dc18af4cd00599a
DBaldwin8 <darren_baldwin@live.com>
2024-12-09 16:22:33 +0000

    added description to login page

--- recipe-finder-fe/src/Pages/Login/index.tsx
+++ recipe-finder-fe/src/Pages/Login/index.tsx
@@ -3,7 +3,7 @@ import SubmitInput from "../../Utilities/SubmitInput";
 function Login() {
   return (
     <div>
-      <p>Put app description here</p>
+      <p>Welcome to the FoodHub. This app is designed to help you match the ingredients in your home to recipes you enjoy! Create an account and login to save your ingredients and recipes.</p>
       <form
         action=""
         className="flex flex-col mx-auto border border-black rounded-md p-4 gap-2 max-w-sm"
