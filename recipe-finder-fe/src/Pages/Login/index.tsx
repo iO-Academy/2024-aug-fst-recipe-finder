b9e64ef88c1b8206b2995aaa8aee8d33074ccf96
DBaldwin8 <darren_baldwin@live.com>
2024-12-09 16:39:45 +0000

    login page text spaced and heading added

--- recipe-finder-fe/src/Pages/Login/index.tsx
+++ recipe-finder-fe/src/Pages/Login/index.tsx
@@ -2,8 +2,9 @@ import SubmitInput from "../../Utilities/SubmitInput";
 
 function Login() {
   return (
-    <div>
-      <p>Welcome to the FoodHub. This app is designed to help you match the ingredients in your home to recipes you enjoy! Create an account and login to save your ingredients and recipes.</p>
+    <div className="my-auto px-4">
+      <h2 className="text-center font-bold my-4">Welcome to the FoodHub.</h2>
+      <p className="text-center my-4"> This app is designed to help you match the ingredients in your home to recipes you enjoy! Create an account and login to save your ingredients and recipes.</p>
       <form
         action=""
         className="flex flex-col mx-auto border border-black rounded-md p-4 gap-2 max-w-sm"
