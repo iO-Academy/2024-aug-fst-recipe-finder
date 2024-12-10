7631b13e13e35e92416eae7a0e51c4f7a97369b6
DBaldwin8 <darren_baldwin@live.com>
2024-12-10 09:55:29 +0000

    changed text on header for login page

--- recipe-finder-fe/src/App.tsx
+++ recipe-finder-fe/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header title="Recipe Maker" />
+      <Header title="FoodHub" />
       <UserContext.Provider
         value={{ userId: currentUserId, changeUserID: setUserId }}
       >
