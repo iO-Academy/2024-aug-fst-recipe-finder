74884cda22028101cf10b3180808c2f3a82276b7
Fred_Rice <fredjrice@outlook.com>
2024-12-10 15:34:01 +0000

    Created route and layout for single recipe page

--- recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
+++ recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
@@ -1,9 +1,27 @@
 // import SubmitInput from "../../Utilities/SubmitInput";
 
 function SingleRecipe() {
-  return (
-    <div className="my-auto mx-auto max-w-md px-4">
-      <h2 className="text-center font-bold my-4">Welcome to the FoodHub.</h2>
+  return ( 
+    <div className="border-4 border-solid border-black rounded-lg w-screen mt-10 file:my-auto mx-auto max-w-md px-4">
+        <h2 className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Single Recipe</h2>
+      <div id="recipecard" className=" p-2 text-center font-bold my-4">
+        <div className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Recipe Name</div>
+        <div id="times" className="flex flex-row justify-between text-center font-bold my-4">
+          <div id="preptime" className="border-2 border-solid border-black rounded px-5 py-2 text-center font-bold my-4">Prep Time: 30 mins</div>
+          <div id="cooktime" className="border-2 border-solid border-black rounded px-5 py-2 text-center font-bold my-4">Cook Time: 30 mins</div>
+        </div>
+        <div id="ingredients" className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Ingredient1</div>
+        <div id="instructions" className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Instructions</div>
+      </div>
+    </div>
+  );
+}
+
+export default SingleRecipe;
+
+
+
+{/* <h2 className="text-center font-bold my-4">Welcome to the FoodHub.</h2>
       <p className="text-center my-4"> This app is designed to help you match the ingredients in your home to recipes you enjoy! Create an account and login to save your ingredients and recipes.</p>
       <form
         action=""
@@ -15,10 +33,4 @@ function SingleRecipe() {
           id="email"
           className="border border-slate-400 rounded-md"
         />
-        <SubmitInput value="Log In" />
-      </form>
-    </div>
-  );
-}
-
-export default SingleRecipe;
+        <SubmitInput value="Log In" /> */}
