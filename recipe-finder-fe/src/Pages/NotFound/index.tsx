a744d1fbcf7c838e967a39c3918d647acfe8b5fd
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-10 10:57:20 +0000

    Add bg image for 404

--- recipe-finder-fe/src/Pages/NotFound/index.tsx
+++ recipe-finder-fe/src/Pages/NotFound/index.tsx
@@ -1,6 +1,7 @@
 function NotFound() {
   return (
-    <div className="text-center flex flex-col flex-grow justify-center">
+    <div className="text-center flex flex-col flex-grow justify-center bg-[url('../public/img/icecream.jpg')] bg-cover bg-center bg-no-repeat">
+      {/* <img src="img/icecream.jpg" alt="" className="" /> */}
       <h1 className="text-6xl font-semibold">404</h1>
       <h2 className="text-3xl">Page not found</h2>
       <p className="text-xl">
