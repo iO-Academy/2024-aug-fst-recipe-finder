1cd7e4c1f7b12065e5b524878f09220d2bb9fd8e
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-10 11:21:02 +0000

    Add bg image to NotFound page

--- recipe-finder-fe/src/Pages/NotFound/index.tsx
+++ recipe-finder-fe/src/Pages/NotFound/index.tsx
@@ -1,15 +1,16 @@
 function NotFound() {
   return (
-    <div className="text-center flex flex-col flex-grow justify-center bg-[url('../public/img/icecream.jpg')] bg-cover bg-center bg-no-repeat">
-      {/* <img src="img/icecream.jpg" alt="" className="" /> */}
-      <h1 className="text-6xl font-semibold">404</h1>
-      <h2 className="text-3xl">Page not found</h2>
-      <p className="text-xl">
-        Try going back to the previous page, or go home{" "}
-        <a href="/" className="text-amber-700">
-          here
-        </a>
-      </p>
+    <div className="text-center flex flex-col items-end flex-grow justify-center bg-[url('../public/img/icecream.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="w-6/12 text-center mr-4 lg:mr-20">
+        <h1 className="text-3xl lg:text-6xl font-semibold">404</h1>
+        <h2 className="text-xl lg:text-3xl">Page not found</h2>
+        <p className="text-md lg:text-xl">
+          Try going back to the previous page, or go home{" "}
+          <a href="/" className="text-amber-700">
+            here
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
