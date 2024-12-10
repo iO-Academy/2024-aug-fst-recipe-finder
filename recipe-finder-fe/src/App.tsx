e9ff0b69b74dfd9048bb57698d531eb8500c2901
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-10 09:09:05 +0000

    working on header component

--- recipe-finder-fe/src/App.tsx
+++ recipe-finder-fe/src/App.tsx
@@ -4,11 +4,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <section className="w-full mt-2">
-        <div className=" md:h-64  bg-cover sm:bg-cover bg-no-repeat bg-[url('../public/img/hero.jpg')] ">
-          <h1 className="text-5xl sm:text-7xl font-sans font-semibold w-full text-center py-16">FoodHub</h1>
-        </div>
-      </section>
     </>
   );
 }
