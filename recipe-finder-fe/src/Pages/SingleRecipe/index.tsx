6378f907429380f0b1159c1558155bb55ba38027
Fred_Rice <fredjrice@outlook.com>
2024-12-10 16:30:22 +0000

    aligned back button and title

--- recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
+++ recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
@@ -2,19 +2,19 @@
 
 function SingleRecipe() {
   return ( 
-    <div className="border-4 border-solid border-black rounded-lg w-screen mt-40 file:my-auto mx-auto max-w-md px-4">
-      <div className="flex flex-row">
-        <button className="border-2 border-solid border-black rounded justify-self-center w-1/2 p-2 text-center font-bold my-4">Back</button>
-        <div className="border-2 border-solid border-black rounded justify-self-center w-1/2 p-2 text-center font-bold my-4">Single Recipe</div>
+    <div className="border-4 border-solid border-black rounded-lg w-screen mt-20 file:my-auto mx-auto max-w-md px-4">
+      <div className="">
+        <button className="inline-flex border-2 border-solid border-black rounded-lg w-1/8 p-2 text-center font-bold mx-2">Back</button>
+        <div className="inline-flex border-2 border-solid border-black rounded-lg w-7/8 p-2 text-center font-bold mx-16 my-4">Single Recipe</div>
       </div>
       <div id="recipecard" className=" p-2 text-center font-bold my-4">
-        <div className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Recipe Name</div>
+        <div className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">Recipe Name</div>
         <div id="times" className="flex flex-row justify-between text-center font-bold my-4">
-          <div id="preptime" className="border-2 border-solid border-black rounded px-5 py-2 text-center font-bold my-4">Prep Time: 30 mins</div>
-          <div id="cooktime" className="border-2 border-solid border-black rounded px-5 py-2 text-center font-bold my-4">Cook Time: 30 mins</div>
+          <div id="preptime" className="border-2 border-solid border-black rounded-lg px-5 py-2 text-center font-bold my-4">Prep Time: 30 mins</div>
+          <div id="cooktime" className="border-2 border-solid border-black rounded-lg px-5 py-2 text-center font-bold my-4">Cook Time: 30 mins</div>
         </div>
-        <div id="ingredients" className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Ingredient1</div>
-        <div id="instructions" className="border-2 border-solid border-black rounded p-2 text-center font-bold my-4">Instructions</div>
+        <div id="ingredients" className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">Ingredient1</div>
+        <div id="instructions" className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">Instructions</div>
       </div>
     </div>
   );
