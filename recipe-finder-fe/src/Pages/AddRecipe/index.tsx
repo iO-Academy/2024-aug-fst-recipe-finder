c27702889dfcd1bac880f9575bb39f5a1aa6c47c
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-11 11:01:06 +0000

    added submit button to add recipe form

--- recipe-finder-fe/src/Pages/AddRecipe/index.tsx
+++ recipe-finder-fe/src/Pages/AddRecipe/index.tsx
@@ -2,32 +2,33 @@ import { useState } from "react";
 import NumberInput from "../../Utilities/NumberInput";
 import Textarea from "../../Utilities/Textarea";
 import TextInput from "../../Utilities/TextInput";
-import ButtonInput from "../../Utilities/Button";
-
+import SubmitInput from "../../Utilities/SubmitInput";
 
 export default function AddRecipe() {
-  const [ingredientsList, setIngredientsList] = useState ([]);
+  const [ingredientsList, setIngredientsList] = useState([]);
   return (
     <>
-      <div className="w-full px-5 mt-4">
-        <TextInput
-          title="Recipe Name"
-          type="text"
-          name="addrecipe"
-          id="addrecipe"
-          placeholder="enter recipe name"
-        />
-        <Textarea title="Cooking instructions" name="intructions" id="instructions" />
-        <div className="flex space-x-4">
-          <NumberInput title="Prep time" name="preptime" id="preptime"  />
-          <NumberInput title="Cook time" name="cooktime" id="cooktime" />
+      <form action="" method="post">
+        <div className="w-full px-5 mt-4">
+          <TextInput
+            title="Recipe Name"
+            type="text"
+            name="addrecipe"
+            id="addrecipe"
+            placeholder="enter recipe name"
+          />
+          <Textarea
+            title="Cooking instructions"
+            name="intructions"
+            id="instructions"
+          />
+          <div className="flex space-x-4">
+            <NumberInput title="Prep time" name="preptime" id="preptime"  />
+            <NumberInput title="Cook time" name="cooktime" id="cooktime" />
+          </div>
+        <SubmitInput value="add recipe" />
         </div>
-        <div className="flex">
-          <TextInput title="Add Ingredient" type="text" name="addingredient" id="addingredient"/>
-          <ButtonInput value="+" width="w-10" height="h-8"/>
-        </div>
-
-      </div>
+      </form>
     </>
   );
 }
