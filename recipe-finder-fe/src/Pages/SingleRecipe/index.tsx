fd0e20257279cf581c15a4abed36479f4310f256
Fred_Rice <fredjrice@outlook.com>
2024-12-11 09:19:07 +0000

    Deleted extraneous code

--- recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
+++ recipe-finder-fe/src/Pages/SingleRecipe/index.tsx
@@ -21,19 +21,3 @@ function SingleRecipe() {
 }
 
 export default SingleRecipe;
-
-
-
-{/* <h2 className="text-center font-bold my-4">Welcome to the FoodHub.</h2>
-      <p className="text-center my-4"> This app is designed to help you match the ingredients in your home to recipes you enjoy! Create an account and login to save your ingredients and recipes.</p>
-      <form
-        action=""
-        className="flex flex-col mx-auto border border-black rounded-md p-4 gap-2 max-w-sm"
-      >
-        <label htmlFor="email">Email Address:</label>
-        <input
-          type="email"
-          id="email"
-          className="border border-slate-400 rounded-md"
-        />
-        <SubmitInput value="Log In" /> */}
