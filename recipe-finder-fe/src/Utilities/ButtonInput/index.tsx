473fa4bff44bed1f59f92d8295f5958932058c22
DBaldwin8 <darren_baldwin@live.com>
2024-12-11 10:35:39 +0000

    removed link to allow more use

---
+++ recipe-finder-fe/src/Utilities/ButtonInput/index.tsx
@@ -0,0 +1,13 @@
+interface ButtonInput {
+    value: string,
+    width?: string;
+    height?: string;
+}
+
+function ButtonInput({  value, width, height  }: ButtonInput) {
+    return (
+        <button className={`border bg-foodHubOrange rounded-md hover:cursor-pointer ${width} ${height}`}>{value}</button>
+    )
+}
+
+export default ButtonInput
