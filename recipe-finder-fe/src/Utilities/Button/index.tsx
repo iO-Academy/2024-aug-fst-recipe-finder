634a7770da759a6b89add366a51e31ae9dead7e2
DBaldwin8 <darren_baldwin@live.com>
2024-12-11 10:44:44 +0000

    button component created,  interface types

--- recipe-finder-fe/src/Utilities/Button/index.tsx
+++ recipe-finder-fe/src/Utilities/Button/index.tsx
@@ -1,13 +1,13 @@
 interface ButtonInput {
     value: string,
-    link: string,
     width?: string;
     height?: string;
 }
 
-function ButtonInput({  value, link, width, height  }: ButtonInput) {
+function ButtonInput({  value, width, height  }: ButtonInput) {
     return (
-        <a href={link}><button className={`border bg-foodHubOrange rounded-md hover:cursor-pointer ${width} ${height}`}>{value}</button></a>
+        <button className={`border bg-foodHubOrange rounded-md hover:cursor-pointer ${width} ${height}`}>{value}</button>
+
     )
 }
 
