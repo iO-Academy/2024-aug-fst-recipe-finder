0755404429776640d0e14288fdba1f3c53cf0f0b
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-11 10:35:37 +0000

    impoted button to add recipe

--- recipe-finder-fe/src/Pages/AddRecipe/index.tsx
+++ recipe-finder-fe/src/Pages/AddRecipe/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import NumberInput from "../../Utilities/NumberInput";
 import Textarea from "../../Utilities/Textarea";
 import TextInput from "../../Utilities/TextInput";
+import ButtonInput from "../../Utilities/Button";
+
 
 export default function AddRecipe() {
+  const [ingredientsList, setIngredientsList] = useState ([]);
   return (
     <>
       <div className="w-full px-5 mt-4">
@@ -18,9 +22,9 @@ export default function AddRecipe() {
           <NumberInput title="Prep time" name="preptime" id="preptime"  />
           <NumberInput title="Cook time" name="cooktime" id="cooktime" />
         </div>
-        <div>
+        <div className="flex">
           <TextInput title="Add Ingredient" type="text" name="addingredient" id="addingredient"/>
-          
+          <ButtonInput value="+" width="w-10" height="h-8"/>
         </div>
 
       </div>
