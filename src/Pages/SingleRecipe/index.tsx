895708af04bcd870beccf4ed06586625e9eee9a6
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-12 10:19:01 +0000

    SingleRecipe page styling

--- src/Pages/SingleRecipe/index.tsx
+++ src/Pages/SingleRecipe/index.tsx
@@ -5,56 +5,71 @@ import UserContext from "../../Contexts/UserContext";
 import { Link, useParams } from "react-router";
 import Button from "../../Utilities/Button";
 
-interface Recipe{
-    name: string,
-    instructions: string,
-    prep_time: number,
-    cook_time: number
+interface Recipe {
+  name: string;
+  instructions: string;
+  prep_time: number;
+  cook_time: number;
 }
-  
-  function SingleRecipe(){
-  const [recipe,setRecipe] = useState({name:'',instructions: '',prep_time: 0, cook_time:0 })
-  const {userId}=useContext(UserContext);
-  const {recipeId} = useParams();
 
-    async function getSingleRecipe(
-      stateSetter: React.Dispatch<React.SetStateAction<Recipe>>
-    ) {
-      const data = await fetch(`${BASE_URL}/users/${userId}/recipes/${recipeId}`,{
+function SingleRecipe() {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    instructions: "",
+    prep_time: 0,
+    cook_time: 0,
+  });
+  const { userId } = useContext(UserContext);
+  const { recipeId } = useParams();
+
+  async function getSingleRecipe(
+    stateSetter: React.Dispatch<React.SetStateAction<Recipe>>
+  ) {
+    const data = await fetch(
+      `${BASE_URL}/users/${userId}/recipes/${recipeId}`,
+      {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-      });
-      const recipe = await data.json();
-      const name = recipe.data.name
-      const instructions = recipe.data.instructions
-      const prep = recipe.data.prep_time
-      const cook = recipe.data.cook_time
-      stateSetter({name:name,instructions:instructions, prep_time:prep, cook_time:cook});
-    }
-  
-    useEffect(() => {
-      getSingleRecipe(setRecipe);
-    },[]) ;
-    return ( 
-      <><Header title="FoodHub" /><div className="border-4 border-solid border-black rounded-lg w-screen mt-20 file:my-auto mx-auto max-w-md px-4">
-        <div className="">
-          <Link to="/"><Button value={"Back"} width={"w-20"} height={"h-10"}></Button></Link>
-          {/* <a href="/"><button className="inline-flex rounded-lg bg-[#e27d08] w-1/8 p-2 text-center font-bold mx-2">Back</button></a> */}
-          <div className="inline-flex border-2 border-solid border-black rounded-lg mx-16 p-2 text-center font-bold my-4">Single Recipe</div>
-        </div>
-        <div id="recipecard" className=" p-2 text-center font-bold my-4">
-          <div className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">{recipe.name}</div>
-          <div id="times" className="flex flex-row justify-between text-center font-bold my-4">
-            <div id="preptime" className="border-2 border-solid border-black rounded-lg px-5 py-2 text-center font-bold my-4">Prep Time: {recipe.cook_time}</div>
-            <div id="cooktime" className="border-2 border-solid border-black rounded-lg px-5 py-2 text-center font-bold my-4">Cook Time: {recipe.prep_time}</div>
-          </div>
-          {/* <div id="ingredients" className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">Ingredient1</div> */}
-          <div id="instructions" className="border-2 border-solid border-black rounded-lg p-2 text-center font-bold my-4">{recipe.instructions}</div>
-        </div>
-      </div></>
+      }
     );
+    const recipe = await data.json();
+    const name = recipe.data.name;
+    const instructions = recipe.data.instructions;
+    const prep = recipe.data.prep_time;
+    const cook = recipe.data.cook_time;
+    stateSetter({
+      name: name,
+      instructions: instructions,
+      prep_time: prep,
+      cook_time: cook,
+    });
   }
-  
-  export default SingleRecipe;
+
+  useEffect(() => {
+    getSingleRecipe(setRecipe);
+  }, []);
+  return (
+    <>
+      <Header title="FoodHub" />
+      <div className=" mt-4 px-4 ">
+        <div className="max-w-6xl mx-auto text-center bg-amber-700 bg-opacity-15 rounded-md shadow-md p-8 mt-4">
+          <div className="text-left">
+            <Link to="/">
+              <Button value={"Back"} width={"w-20"} height={"h-10"}></Button>
+            </Link>
+          </div>
+          <h1 className="text-3xl">{recipe.name} Recipe Title</h1>
+          <div className="flex justify-center gap-8 mb-10 mt-4">
+            <p>{recipe.prep_time}</p>
+            <p>{recipe.cook_time}</p>
+          </div>
+          <p className="text-left">{recipe.instructions}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SingleRecipe;
