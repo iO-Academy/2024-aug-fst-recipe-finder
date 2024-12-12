420875e5279fab801e86ba67ff1f800c48bc781a
DBaldwin8 <darren_baldwin@live.com>
2024-12-12 10:29:55 +0000

    updated route address and the the file that points to it

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login></Login>} />
             <Route
-              path="recipes/:currentUserId"
+              path=":currentUserId"
               element={<AllRecipes></AllRecipes>}
             />
             <Route path="*" element={<NotFound />} />
