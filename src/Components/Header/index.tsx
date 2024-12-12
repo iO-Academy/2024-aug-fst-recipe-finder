d0ff4a577c38b24b95035831aa8cbf16a3732fb4
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-12 16:13:35 +0000

    Display ingredients on SingleIngredient page

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header({ title }: HeaderProps) {
   return (
     <>
       <section className="w-full">
-        <div className=" md:h-64 h-36  bg-cover sm:bg-cover bg-no-repeat bg-[url('../public/img/hero.jpg')] ">
+        <div className=" md:h-64 h-36  bg-cover sm:bg-cover bg-no-repeat bg-[url('../img/hero.jpg')] ">
           <h1 className="text-5xl sm:text-7xl font-sans font-semibold w-full text-center py-12 md:py-20 text-gray-900">
             {title}
           </h1>
--- src/Pages/SingleRecipe/index.tsx
+++ src/Pages/SingleRecipe/index.tsx
@@ -10,17 +10,24 @@ interface Recipe {
   instructions: string;
   prep_time: number;
   cook_time: number;
+  ingredients: {
+    id: number;
+    name: string;
+  }[];
 }
 
 function SingleRecipe() {
-  const [recipe, setRecipe] = useState({
+  const [recipe, setRecipe] = useState<Recipe>({
     name: "",
     instructions: "",
     prep_time: 0,
     cook_time: 0,
+    ingredients: [],
   });
+
   const { userId } = useContext(UserContext);
   const { recipeId } = useParams();
+  const ingredients = recipe.ingredients;
 
   async function getSingleRecipe(
     stateSetter: React.Dispatch<React.SetStateAction<Recipe>>
@@ -31,17 +38,22 @@ function SingleRecipe() {
     const instructions = recipe.data.instructions;
     const prep = recipe.data.prep_time;
     const cook = recipe.data.cook_time;
+    const ingredients = recipe.data.ingredients;
     stateSetter({
       name: name,
       instructions: instructions,
       prep_time: prep,
       cook_time: cook,
+      ingredients: ingredients,
     });
   }
 
   useEffect(() => {
     getSingleRecipe(setRecipe);
   }, []);
+
+  console.log(ingredients);
+
   return (
     <>
       <Header title="FoodHub" />
@@ -57,6 +69,12 @@ function SingleRecipe() {
             <div className="flex justify-center gap-8 mb-10 mt-4">
               <p>Prep Time: {recipe.prep_time} min(s)</p>
               <p>Cooking Time: {recipe.cook_time} min(s)</p>
+            </div>
+            <div className="mb-8">
+              <strong>Ingredients</strong>
+              {recipe.ingredients.map((ingredient) => {
+                return <p>{ingredient.name}</p>;
+              })}
             </div>
             <p className="text-left">{recipe.instructions}</p>
           </div>
