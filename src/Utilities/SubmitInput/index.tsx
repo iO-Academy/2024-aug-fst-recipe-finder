c4ec3300c66d4de6e8b9223e1d916247b6dd6862
DBaldwin8 <darren_baldwin@live.com>
2024-12-12 10:39:47 +0000

    added lighter orange to a hover

--- src/Utilities/SubmitInput/index.tsx
+++ src/Utilities/SubmitInput/index.tsx
@@ -6,7 +6,7 @@ function SubmitInput({ value }: SubmitInput) {
   return (
     <input
       type="submit"
-      className="border bg-foodHubOrange rounded-md hover:cursor-pointer py-2 px-3"
+      className="border bg-foodHubOrange rounded-md hover:cursor-pointer hover:bg-orange-400 py-2 px-3"
       value={value}
     />
   );
