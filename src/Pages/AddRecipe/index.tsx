3a2ab8a772e604e7a48dedc5b713bf1f29031ceb
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-13 09:31:38 +0000

    Fix Add ingredient title

--- src/Pages/AddRecipe/index.tsx
+++ src/Pages/AddRecipe/index.tsx
@@ -124,10 +124,10 @@ function AddRecipe() {
         </div>
       </form>
       <div className="w-full px-5 mt-4">
-        <p className="mb-2">Add Ingredient</p>
         <form action="" method="post" onSubmit={handleIngredientSubmit}>
           <div className="flex space-x-4">
             <TextInput
+              title="Add Ingredient:"
               name="ingredient"
               id="addingredient"
               placeholder="enter ingredient name"
