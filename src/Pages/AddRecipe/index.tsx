259ecabc10d192b7da39dd04100e9ffa2f059798
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-12-13 09:30:01 +0000

    Fix back button on AddRecipe page

--- src/Pages/AddRecipe/index.tsx
+++ src/Pages/AddRecipe/index.tsx
@@ -89,7 +89,7 @@ function AddRecipe() {
   return (
     <>
       <Header title="Add Recipe" />
-      <Link className="ml-5 mt-4" to={`/recipes/${userId}`}>
+      <Link className="ml-5 mt-4" to={`/recipes`}>
         <Button value="Back" width="w-20" height="h-10" />
       </Link>
       <form action="" method="post" onSubmit={handleSubmit}>
